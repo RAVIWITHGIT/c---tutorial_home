a434f78e20a913ddb1ae8ed95c9bd61872b2ed25
ravi lakshakar <ravilakshakar336@gmail.com>
2024-01-04 21:30:47 +0530

    for loop , while loop , do-while loop

---
+++ tut10/main.cpp
@@ -0,0 +1,65 @@
+// ------------->>>>>>>>>>>>>>>> Loop in c++ <<<<<<<<<<<<<<<<<<<<<<<<<<<----------------------------
+
+/*
+1. There are three types of loops in c++
+    I. for loop --------->
+    syntex of for loop
+    for(initialization; condition; updation){
+        loop body(c++ code)
+    }
+
+    II. while loop ----->
+
+    syntex--
+    while(condtion){
+        c++ statements;
+    }
+
+
+    III. do-while loop --------->>>>
+    do{
+        c++ statements;
+    }while(condtion);
+
+*/
+
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// int main(){
+//     for (int i=1; i<=40; i++){
+//         cout<<i<<endl;
+//     }
+// }
+
+// int main(){
+//     int i = 1;
+//     while(i<=10){
+//         cout<<i<<endl;
+//         i++;
+
+//     }
+// }
+
+
+// --------------------- do while loop
+
+// int main(){
+//     int i = 10;
+//     do{
+//         cout<<i<<endl;
+//         i++;
+//     }while(i>20);
+// }
+
+
+// int main(){
+//     int i=0;
+//     do{
+//         cout<<i<<endl;
+//         i++;
+//     }while(i<=20);
+// }
