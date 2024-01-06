ee7774ffcc1304339122ce12184f476b015bd4cf
ravi lakshakar <ravilakshakar336@gmail.com>
2024-01-06 13:33:09 +0530

    Break and Continue Statment

---
+++ tut11/main.cpp
@@ -0,0 +1,35 @@
+// ---------------------------- Break and Continue Statment ----------------------------------------
+
+
+// ---------------- Break Statment
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main(){
+//     for(int i=1;i<=5;i++){
+//         if(i==3){
+//             break;
+//         }else{
+//             cout<<i<<endl;
+//         }
+//     }
+//     return 0;
+// }
+
+
+// ---------------- continue Statment
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main(){
+//     for(int i=1;i<=5;i++){
+//         if(i==3){
+//             continue;
+//         }else{
+//             cout<<i<<endl;
+//         }
+//     }
+//     return 0;
+// }
