046b499bdf85d359cb8c5823ac5f31bd840627b9
ravi lakshakar <ravilakshakar336@gmail.com>
2024-01-09 21:43:38 +0530

    Array and Pointer Arithmetic

---
+++ tut13/main.cpp
@@ -0,0 +1,71 @@
+// ------------------------- Array and Pointer Arithmetic -------------------------------------------
+
+// ------------------- Array
+#include <bits/stdc++.h>
+using namespace std;
+
+// int main(){
+//     string arr[4] = {"ravi","shiv","ranu","pooja"};
+//     cout<<arr[0]<<endl;
+//     cout<<arr[1]<<endl;
+//     cout<<arr[2]<<endl;
+//     cout<<arr[3]<<endl;
+//     return 0;
+// }
+
+// int main(){
+//     string arr[4] = {};
+//     arr[0] = "ravi";
+//     arr[1] = "ranu";
+//     arr[2] = "shiv";
+//     arr[3] = "pooja";
+//     cout<<arr[0]<<endl;
+//     cout<<arr[1]<<endl;
+//     cout<<arr[2]<<endl;
+//     cout<<arr[3]<<endl;
+//     return 0;
+// }
+
+// -- update array value
+// int main(){
+//     string arr[4] = {"ravi","shiv","ranu","pooja"};
+//     cout<<arr[0]<<endl;
+//     arr[1] = "babu";
+//     cout<<arr[1]<<endl;
+//     cout<<arr[2]<<endl;
+//     cout<<arr[3]<<endl;
+//     return 0;
+// }
+
+
+// --- iterate using for loop , while loop and do while loop
+// int main(){
+//     string arr[4] = {"ravi","shiv","ranu","pooja"};
+//     for (int i = 0; i < 4; i++)
+//     {
+//         cout<<arr[i]<<endl;
+//     }
+    
+//     return 0;
+// }
+
+
+// int main(){
+//     int arr[4] = {1,2,3,4};
+//     int i = 0;
+//     while(i<4){
+//         cout<<arr[i]<<endl;
+//         i++;
+//     }
+// }
+
+// int main(){
+//     int arr[4] = {1,2,3,4};
+//     int i = 0;
+//     do{
+//         cout<<arr[i]<<endl;
+//         i++;
+//     }while(i<4);
+// }
+
+
